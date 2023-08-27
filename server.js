f25d98ffd5f834e4c7a09c5b577c360a241ec86e
ajstyza <124314058+ajstyza@users.noreply.github.com>
2023-08-27 14:55:32 -0500

    added delete route in serve.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {MongoClient} = require('mongodb');
+const {MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
 const port = 3001;
@@ -11,7 +11,7 @@ const client = new MongoClient(connectionStringURI);
 let db;
 
 // need to add dbName
-const dbName = ''
+const dbName = 'userDB'
 
 client.connect()
 .then(() => {
@@ -20,7 +20,7 @@ client.connect()
 // client constructor to add new db instance
     db = client.db(dbName);
 
-    AutomaticPrefetchPlugin.listen(port, () => {
+    app.listen(port, () => {
         console.log(`app listening at http://localhost:${port}`);
     });
 })
@@ -31,9 +31,21 @@ client.connect()
 // Built in Express function that parses incoming requests to JSON
 app.use(express.json());
 
+// may also need create-many to post mutiple things
+app.post('/create-many', (req, res) => {
+db.collection('usersCollection').insertMany([
+    {"tite": "balfsdafds"},
+    {"title": "insevdv"}
+])
+.then(results => res.json(results))
+.catch(err => {
+  if (err) throw err;
+})
+});
+
 app.post('/create', (req, res) => {
   // Use db connection to add a document
-  db.collection('petCollection').insertOne(
+  db.collection('usersCollection').insertOne(
     { name: req.body.name, email: req.body.email, thoughts: req.body.thoughts, firends: req.body.friends}
   )
     .then(results => res.json(results))
@@ -44,10 +56,30 @@ app.post('/create', (req, res) => {
 
 app.get('/read', (req, res) => {
   // Use db connection to find all documents in collection
-  db.collection('userCollection')
+  db.collection('usersCollection')
     .find()
     .toArray()
     .then(results => res.json(results))
+    .catch(err => {
+      if (err) throw err;
+    });
+});
+
+app.delete('/delete', (req, res) => {
+  // Wrap the id in the ObjectId class to instantiate a new instance
+  const userId = new ObjectId(req.body.id);
+
+  // Use deleteOne() to delete one object
+  db.collection('usersCollection').deleteOne(
+    // This is the filter. We delete only the document that matches the _id provided in the request body.
+    { _id: userId }
+  )
+    .then(results => {
+      console.log(results);
+      res.send(
+        results.deletedCount ? 'User deleted' : 'No user found!'
+      );
+    })
     .catch(err => {
       if (err) throw err;
     });
